2b95e1c95ada348621d069068f86f41775bcab53
Thorsten Deinert <thorsten.deinert@udo.edu>
2017-07-31 14:52:35 +0200

    Translated comments

--- lib/patches/pdf_attach_for_external_files.rb
+++ lib/patches/pdf_attach_for_external_files.rb
@@ -1,16 +1,16 @@
 module PdfExtensions
   module Patches::PdfAttachForExternalFiles
     def attach(attachments, filename, encoding)
-      # Original-Routine machen lassen. Wenn sie ein Attachment findet, einfach zurückgeben.
+      # Let Redmine do the work first. If it finds an attachment, we're all done.
       attachment = super(attachments, filename, encoding)
       return attachment if attachment
 
-      # Es wurde kein Attachment innerhalb des Tickets gefunden. Weitersuchen...
+      # There was no corresponding attachment in our object (issue). We'll look for it in all attachments...
 
-      # Entspricht der Dateiname einer Attachment-URL?  Die ID wird als erste Gruppe gematcht.
+      # is our filename a route to an attachment? Extract the id as first pattern match group
       /attachments\/download\/(\d+)\/.*\.(gif|jpeg|jpg|png)\z/.match(filename) do |match|
-        # Das Attachment laden, deren ID gematcht wurde.
-        # ACHTUNG: match[0] ist der gesamte Match, match[1] daher die erste Gruppe.
+        # Load attachment with the matched id from databse
+        # Hint: match[0] is the whole match, match[1] is the first group, our id
         attachment = Attachment.find(match[1])
 
         if attachment && attachment.readable? && attachment.visible?
@@ -20,7 +20,7 @@ module PdfExtensions
         end
       end
 
-      # Es passte kein Attachment zum Dateinamen. Die Datei wird nicht in de PDF eingebunden.
+      # No attachment found, so return nil. The image file won't be loaded, just the filename is rendered into the pdf
       nil
     end
   end
